6d6be26e66ac704a5a27530ddad73a1e78019ab5
samridhi1408 <samridhim1408@gmail.com>
2026-04-19 11:32:20 -0400

    Updated file pushed.

--- motif_analysis/run_homer.sh
+++ motif_analysis/run_homer.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
-#SBATCH -J enrichment
+#SBATCH -J enrichment_all
 #SBATCH -p RM-shared
 #SBATCH -N 1
-#SBATCH -n 2
+#SBATCH -n 8  
 #SBATCH -t 12:00:00
 #SBATCH --mem-per-cpu=2000M
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=smakkar@andrew.cmu.edu
 
-INPUT_BED="filepath"
-OUTPUT_DIR="/jet/home/smakkar/enrichment/results"
-
 export PATH=$PATH:/jet/home/smakkar/enrichment/homer/bin/
 
-mkdir -p $OUTPUT_DIR
+#directories
+INPUT_DIR="/jet/home/smakkar/enrichment/raw_results"
+BASE_OUT="/jet/home/smakkar/enrichment/results"
 
-#subject to change for the reference genome and the motif size
-findMotifsGenome.pl $INPUT_BED hg18 $OUTPUT_DIR -size 200 -mask -p 2
+mkdir -p $BASE_OUT/human_promoters
+mkdir -p $BASE_OUT/human_enhancers
+
+echo "Starting all HOMER analyses concurrently"
+
+# Human Promoters (hg38)
+findMotifsGenome.pl $INPUT_DIR/human_promoters.bed hg38 $BASE_OUT/human_promoters -size 200 -mask -p 2 &
+
+# Human Enhancers (hg38)
+findMotifsGenome.pl $INPUT_DIR/human_enhancers.bed hg38 $BASE_OUT/human_enhancers -size 200 -mask -p 2 &
+
+
+wait
+
+echo "All motif analyses complete!"
